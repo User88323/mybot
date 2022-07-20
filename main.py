4a2f65eefe4125f990fc1c28a3806f542ae47b69
User88323 <108779433+User88323@users.noreply.github.com>
2022-07-20 16:45:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from details import api_id, api_hash, bot_token
 import tgcrypto
 import requests
 import os
-import fun
 
 bot = Client("my bot",
             api_id = api_id,
@@ -13,8 +12,21 @@ bot = Client("my bot",
 @bot.on_message(filters.command(['start']) & filters.private)
 
 def welcome(Client,message):
-    fun.download("https://file2directlink.herokuapp.com/65068823684674195530940540/AgADCAUA/2_5350717343083272678.mp4")
+    download("https://file2directlink.herokuapp.com/65068823684674195530940540/AgADCAUA/2_5350717343083272678.mp4")
     print("im from end")
     message.reply_video(video=file_name)
-    #os.remove(file_name)
+    os.remove(file_name)
+
+def download(url):
+    get_response = requests.get(url,stream=True)
+    file_name  = url.split("/")[-1]
+    with open(file_name, 'wb') as f:
+        for chunk in get_response.iter_content(chunk_size=1024):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+    
+    print(file_name)
+    return file_name
+
+            
 bot.run()
