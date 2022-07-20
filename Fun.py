ad600bce0c432f7c6545297f71ef2aea53213a2e
User88323 <108779433+User88323@users.noreply.github.com>
2022-07-20 16:09:57 +0530

    Update Fun.py

--- Fun.py
+++ Fun.py
@@ -1,43 +1,12 @@
-import requests
-import os
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
-def DetectFileSize(url):
-    r = requests.get(url, allow_redirects=True, stream=True)
-    return int(r.headers.get("content-length", 0))
-
-
-
-
-
-def DownLoadFile(url, file_name, chunk_size, client, ud_type, message_id, chat_id):
-    if os.path.exists(file_name):
-        os.remove(file_name)
-    if not url:
-        return file_name
-    r = requests.get(url, allow_redirects=True, stream=True)
-    # https://stackoverflow.com/a/47342052/4723940
-    total_size = int(r.headers.get("content-length", 0))
-    downloaded_size = 0
-    with open(file_name, 'wb') as fd:
-        for chunk in r.iter_content(chunk_size=chunk_size):
-            if chunk:
-                fd.write(chunk)
-                downloaded_size += chunk_size
-            if (
-                client is not None
-                and ((total_size // downloaded_size) % 5) == 0
-            ):
-                time.sleep(0.3)
-                try:
-                    client.edit_message_text(
-                        chat_id,
-                        message_id,
-                        text=f"{ud_type}: {humanbytes(downloaded_size)} of {humanbytes(total_size)}",
-                    )
-                except:
-                    pass
+import os,requests
+def download(url):
+    get_response = requests.get(url,stream=True)
+    file_name  = url.split("/")[-1]
+    with open(file_name, 'wb') as f:
+        for chunk in get_response.iter_content(chunk_size=1024):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
     return file_name
+
+
+download("https://file2directlink.herokuapp.com/65068823684674195530940540/AgADCAUA/2_5350717343083272678.mp4")
