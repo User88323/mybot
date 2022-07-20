135dcc7cfae36b0dc3a0649d3eacfc41de08aa4a
User88323 <108779433+User88323@users.noreply.github.com>
2022-07-20 16:39:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 import fun
 
 bot = Client("my bot",
-            api_id = api_id),
+            api_id = api_id,
             api_hash = api_hash,
             bot_token = bot_token)
 @bot.on_message(filters.command(['start']) & filters.private)
@@ -16,5 +16,5 @@ def welcome(Client,message):
     fun.download("https://file2directlink.herokuapp.com/65068823684674195530940540/AgADCAUA/2_5350717343083272678.mp4")
     print("im from end")
     message.reply_video(video=file_name)
-    os.remove(file_name)
+    #os.remove(file_name)
 bot.run()
