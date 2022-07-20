2e416facd74100687a164abf3d7379b15390641e
User88323 <108779433+User88323@users.noreply.github.com>
2022-07-20 14:28:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from details import api_id, api_hash, bot_token
 import tgcrypto
 
 bot = Client("my bot",
-            api_id = api_id,
-            api_hash = api_hash,
-            bot_token = bot_token)
+            api_id = int(os.environ["API_ID"]),
+            api_hash = os.environ["API_HASH"],
+            bot_token = os.environ["BOT_TOKEN"])
 @bot.on_message(filters.command(['start']) & filters.private)
 
 def welcome(Client,message):
