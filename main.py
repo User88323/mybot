7a019eaf4bd8100ea30f18bcb27b0a1611ca97a2
User88323 <108779433+User88323@users.noreply.github.com>
2022-07-24 13:49:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def welcome(Client,message):
     url = "https://file2directlink.herokuapp.com/65686715824167183548198464/AgADtiEA/Bhool.Bhulaiyaa.2.2022.PreDvd.Hindi.480p_mkvCinemas.mkv"
 
     file_name  = download(url)
-    #url = "https://file2directlink.herokuapp.com/65068823684674195530940540/AgADCAUA/2_5350717343083272678.mp4"
+    #url = "https://www.pexels.com/video/5532774/download/"
     download(url)
     print("im from end")
     message.reply_video(file_name)
