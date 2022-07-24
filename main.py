fecd87366e86e0c251c119dba99e6b357b1e2b64
User88323 <108779433+User88323@users.noreply.github.com>
2022-07-24 13:38:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import tgcrypto
 import requests
 import os
 import time
+from urllib.parse import urlparse
 
 bot = Client("my bot",
             api_id = api_id,
@@ -16,7 +17,7 @@ def welcome(Client,message):
     message.reply_text(text="hi")
     url = "https://file2directlink.herokuapp.com/65686715824167183548198464/AgADtiEA/Bhool.Bhulaiyaa.2.2022.PreDvd.Hindi.480p_mkvCinemas.mkv"
 
-    file_name  = url.split("/")[-1]
+    file_name  = download(url)
     #url = "https://file2directlink.herokuapp.com/65068823684674195530940540/AgADCAUA/2_5350717343083272678.mp4"
     download(url)
     print("im from end")
@@ -25,16 +26,21 @@ def welcome(Client,message):
     
     #os.remove(file_name)
     print("endd")
+
 def download(url):
+    a = urlparse(url)
+    print(a.path)
+    file_name = os.path.basename(a.path)
+
+  
     get_response = requests.get(url,stream=True)
-    file_name  = url.split("/")[-1]
+    #f#le_name  = url.split("/")[-1]
     with open(file_name, 'wb') as f:
         for chunk in get_response.iter_content(chunk_size=1024):
             if chunk: # filter out keep-alive new chunks
                 f.write(chunk)
-    
-    print(file_name)
     return file_name
+
 
             
 bot.run()
