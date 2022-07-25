d341cefeff8b75963e8ae2f712b0e22ea674c826
User88323 <108779433+User88323@users.noreply.github.com>
2022-07-25 22:27:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ def welcome(Client,message):
     url = "https://file2directlink.herokuapp.com/68713678952710318415642748/AgADVBAA/Ek_Paheli_Leela_2015_WebRip_Hindi_480p_ESub_mkvCinemas.mkv"
 
     #file_name  = url.split("/")[-1]
-    download(url)
-    file_name1 = download(url)
+    file_namex = download(url)
+    
     #download(url)
     print("iim from end")
-    message.reply_video(file_name1)
+    message.reply_video(file_namex)
     
     os.remove(file_name1)
     print("endd")
