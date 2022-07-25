adba7f4c6465df2374c8862a1c259516d5f12793
User88323 <108779433+User88323@users.noreply.github.com>
2022-07-25 21:06:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -52,18 +52,18 @@ def fetcher(pagel):
     lod = 0
     for link in ancr:
     	if (link.get('href').find("/video/") != -1):
-    	link = "https://ok.xxx/"+link.get('href')
-    	r = requests.get(link)
-    	source2 = r.content
-    	soup2 = BeautifulSoup(source2,'html.parser')
-    	div = soup2. find("a", {"class": "download-link"})
-    	cn = str(div)
-    	direct_l = cn[cn.find("http") : cn.find("style") - 2]
-    	output.append(direct_l)
-    	#update.message.reply_text(direct_l+"                          "+"Original_Link: "+link)
-    	lod = lod +1 
-    	print('____----______--- ' + str(lod) + '---_____---_____')
-    	print("\033c")
+            link = "https://ok.xxx/"+link.get('href')
+            r = requests.get(link)
+    	    source2 = r.content
+    	    soup2 = BeautifulSoup(source2,'html.parser')
+    	    div = soup2. find("a", {"class": "download-link"})
+    	    cn = str(div)
+    	    direct_l = cn[cn.find("http") : cn.find("style") - 2]
+    	    output.append(direct_l)
+    	    #update.message.reply_text(direct_l+"                          "+"Original_Link: "+link)
+    	    lod = lod +1 
+    	    print('____----______--- ' + str(lod) + '---_____---_____')
+    	    print("\033c")
 return direct_l
             
 bot.run()
